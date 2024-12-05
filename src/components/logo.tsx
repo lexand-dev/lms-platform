5198abf42e3086a81c82c6137859e92cbe4098dd
alexander flores <alexfloresrayme@gmail.com>
2024-12-04 23:56:38 -0500

    fix: update logo image path to use absolute URL

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export const Logo = () => {
-  return <Image src="logo.svg" alt="Logo" width={130} height={130} />;
+  return <Image src="/logo.svg" alt="Logo" width={130} height={130} />;
 };
